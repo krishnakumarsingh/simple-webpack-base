d3d255033810a91e86c165698b780ea0d9dd93a4
krishnakumarsingh <krishnakumarsingh@users.noreply.github.com>
2020-11-19 18:05:07 +0530

    Update config.prod.js

--- config.prod.js
+++ config.prod.js
@@ -1,23 +1,38 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: {
     myfile: "./src/index.js",
   },
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "js/[name].js",
-    publicPath: "/assets/",
+    /* publicPath: "/assets/", */
     chunkFilename: "[name].bundle.js",
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "less-loader",
+            options: {
+              additionalData: `@env: ${process.env.NODE_ENV};`,
+            },
+          },
+        ],
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
   optimization: {},
 };
