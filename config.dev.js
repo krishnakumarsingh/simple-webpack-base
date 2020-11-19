1d9975434960b609d7766fa05205c3b52368d362
krishnakumarsingh <krishnakumarsingh@users.noreply.github.com>
2020-11-19 18:05:37 +0530

    Update config.dev.js

--- config.dev.js
+++ config.dev.js
@@ -1,11 +1,14 @@
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
     myfile: "./src/index.js",
   },
   output: {
-    path: path.resolve(__dirname, "build"),
+    /* path: path.resolve(__dirname, "build"), */
     filename: "js/[name].js",
     publicPath: "/assets/",
   },
@@ -32,5 +35,6 @@ module.exports = {
     writeToDisk: false,
     hot: true,
   },
+  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin()],
   optimization: {},
 };
